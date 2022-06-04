acfd405fa870d28eec4f0bcba9e61f448be0ecf7
shane <shaneaudinett@gmail.com>
2022-06-04 07:28:29 +0100

    initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,57 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import Vue from 'vue'
+// import VueRouter from 'vue-router'
+// import HomeView from '../views/HomeView.vue'
 
-Vue.use(VueRouter)
+// Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+// const routes = [
+//   {
+//     path: '/',
+//     name: 'home',
+//     component: HomeView
+//   },
+//   {
+//     path: '/about',
+//     name: 'about',
+//     // route level code-splitting
+//     // this generates a separate chunk (about.[hash].js) for this route
+//     // which is lazy-loaded when the route is visited.
+//     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+//   }
+// ]
 
-const router = new VueRouter({
+// const router = new VueRouter({
+//   mode: 'history',
+//   base: process.env.BASE_URL,
+//   routes
+// })
+
+// export default router
+
+import Vue from 'vue';
+import Router from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+
+Vue.use(Router);
+
+export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
-})
-
-export default router
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/event/:id',
+      name: 'eventSingle',
+      component: () => import('../views/EventSingle.vue'),
+    },
+  ],
+});
